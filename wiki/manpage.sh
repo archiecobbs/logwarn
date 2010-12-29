a207a80993f61a42e371fd6de16d6da0e8447c62
archie.cobbs <archie.cobbs@6c1839cf-45f0-7c72-a366-02261d637c91>
2010-12-29 19:15:18 +0000

    - Fix bug in manpage script. - Generate man page.

--- wiki/manpage.sh
+++ wiki/manpage.sh
@@ -11,7 +11,7 @@ if [ `uname -s` = 'Darwin' ]; then
     echo This doesn\'t work on Mac OS 1>&2
     exit 1
 fi
-groff -r LL=100n -r LT=100n -Tlatin1 -man ../trunk/logwarn.1 | sed "${SEDFLAG}" -e 's/.\x08(.)/\1/g' -e 's/[[0-9]+m//g' 
+groff -r LL=100n -r LT=100n -Tlatin1 -man ../trunk/logwarn.1 | sed -r -e 's/.\x08(.)/\1/g' -e 's/[[0-9]+m//g' 
 
 cat << "xxEOFxx"
 }}}
