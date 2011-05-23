38b72e1355db80bcbb9e74fea683a4cec37c241c
archie.cobbs <archie.cobbs@6c1839cf-45f0-7c72-a366-02261d637c91>
2011-05-23 15:49:57 +0000

    Clean up logwarn.spec now that it's a generated file.

--- trunk/cleanup.sh
+++ trunk/cleanup.sh
@@ -14,6 +14,6 @@ rm -f scripts
 find . \( -name Makefile -o -name Makefile.in \) -print0 | xargs -0 rm -f
 rm -f svnrev.c
 rm -f *.o logwarn logwarn.1
-rm -f logwarn-*.tar.gz
+rm -f logwarn-*.tar.gz logwarn.spec
 rm -rf a.out.* tags
 
