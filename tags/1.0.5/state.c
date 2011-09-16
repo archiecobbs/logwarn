28cdb06af0c7d5f24c34125e93fbf3700ff0f9e7
archie.cobbs <archie.cobbs@6c1839cf-45f0-7c72-a366-02261d637c91>
2011-09-16 02:51:00 +0000

    Tagging release 1.0.5.

---
+++ tags/1.0.5/state.c
@@ -0,0 +1,189 @@
+
+/*
+ * Logwarn - Utility for finding interesting messages in log files
+ *
+ * Copyright (C) 2010-2011 Archie L. Cobbs. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * $Id$
+ */
+
+#include "config.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <ctype.h>
+#include <err.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "logwarn.h"
+
+// Definitions
+#define INODENUM_NAME       "INODENUM"
+#define LINENUM_NAME        "LINENUM"
+#define POSITION_NAME       "POSITION"
+#define MATCHING_NAME       "MATCHING"
+#define STDIN_LOGFILE_NAME  "_stdin"
+
+int
+load_state(const char *state_file, struct scan_state *state)
+{
+    char buf[1024];
+    struct stat sb;
+    FILE *fp;
+
+    memset(state, 0, sizeof(*state));
+    state->line = 1;
+    if (stat(state_file, &sb) == -1 || S_ISDIR(sb.st_mode))
+        return -1;
+    if ((fp = fopen(state_file, "r")) == NULL)
+        return -1;
+    while (fgets(buf, sizeof(buf), fp) != NULL) {
+        const char *s = buf;
+        unsigned long value;
+        const char *fname;
+        const char *fvalue;
+        char *eptr;
+        char *t;
+
+        // Ignore blank lines and comments
+        while (isspace(*s))
+            s++;
+        if (*s == '\0' || *s == '#')
+            continue;
+
+        // Parse name and value
+        if ((t = strchr(s, '=')) == NULL)
+            continue;
+        fname = s;
+        *t++ = '\0';
+        if (*t != '"')
+            continue;
+        fvalue = ++t;
+        if ((t = strchr(t, '"')) == NULL)
+            continue;
+        *t = '\0';
+
+        // Handle "false" and "true"
+        if (strcmp(fvalue, "false") == 0)
+            fvalue = "0";
+        else if (strcmp(fvalue, "true") == 0)
+            fvalue = "1";
+
+        // Decode numerical value
+        if (((value = strtoul(fvalue, &eptr, 10)) == ULONG_MAX && errno == ERANGE) || *eptr != '\0') {
+            warnx("can't decode value \"%s\" for \"%s\"", fvalue, fname);
+            continue;
+        }
+
+        // Update state
+        if (strcmp(fname, INODENUM_NAME) == 0)
+            state->inode = value;
+        else if (strcmp(fname, LINENUM_NAME) == 0)
+            state->line = value;
+        else if (strcmp(fname, POSITION_NAME) == 0)
+            state->pos = value;
+        else if (strcmp(fname, MATCHING_NAME) == 0)
+            state->matching = value != 0;
+    }
+    (void)fclose(fp);
+    return 0;
+}
+
+void
+save_state(const char *state_file, const char *logfile, const struct scan_state *state)
+{
+    FILE *fp;
+
+    if ((fp = fopen(state_file, "w")) == NULL)
+        err(EXIT_ERROR, "%s", state_file);
+    dump_state(fp, logfile, state);
+    if (fclose(fp) == EOF)
+        err(EXIT_ERROR, "%s", state_file);
+}
+
+void
+dump_state(FILE *fp, const char *logfile, const struct scan_state *state)
+{
+    struct scan_state stdin_state;
+
+    if (logfile == NULL) {
+        logfile = STDIN_LOGFILE_NAME;
+        stdin_state = *state;
+        stdin_state.inode = 0;
+        state = &stdin_state;
+    }
+    fprintf(fp, "# %s %s state for \"%s\"\n", PACKAGE_TARNAME, PACKAGE_VERSION, logfile);
+    fprintf(fp, "%s=\"%lu\"\n", INODENUM_NAME, (unsigned long)state->inode);
+    fprintf(fp, "%s=\"%lu\"\n", LINENUM_NAME, state->line);
+    fprintf(fp, "%s=\"%lu\"\n", POSITION_NAME, state->pos);
+    fprintf(fp, "%s=\"%s\"\n", MATCHING_NAME, state->matching ? "true" : "false");
+}
+
+void
+init_state_from_logfile(const char *logfile, struct scan_state *state)
+{
+    struct stat sb;
+    FILE *fp;
+    int ch;
+
+    memset(state, 0, sizeof(*state));
+    state->line = 1;
+    if (logfile == NULL)
+        return;
+    if (stat(logfile, &sb) == -1)
+        err(EXIT_ERROR, "%s", logfile);
+    if (S_ISDIR(sb.st_mode & S_IFMT)) {
+        errno = EISDIR;
+        err(EXIT_ERROR, "%s", logfile);
+    }
+    state->inode = sb.st_ino;
+    if ((fp = fopen(logfile, "r")) == NULL)
+        err(EXIT_ERROR, "%s", logfile);
+    while ((ch = getc(fp)) != EOF) {
+        state->pos++;
+        if (ch == '\n')
+            state->line++;
+    }
+    (void)fclose(fp);
+}
+
+void
+state_file_name(const char *state_dir, const char *logfile, char *buf, size_t max)
+{
+    int i;
+
+    if (logfile == NULL) {
+        snprintf(buf, max, "%s", STDIN_LOGFILE_NAME);
+        return;
+    }
+    snprintf(buf, max, "%s/", state_dir);
+    while (*logfile == '/')
+        logfile++;
+    for (i = strlen(buf); i < max - 1 && *logfile != '\0'; ) {
+        char ch = *logfile++;
+
+        if (ch == '/')
+            ch = '_';
+        buf[i++] = ch;
+        buf[i] = '\0';
+    }
+}
+
---
+++ tags/1.0.5/tests/testutil.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+# Common definitions
+NAME=`basename $0`
+TESTDIR=`pwd`
+LOGWARN="${TESTDIR}/../logwarn"
+
+# Log functions
+log()
+{
+    echo ${NAME}: ${1+"$@"} 1>&2
+}
+
+# Error function
+errout()
+{
+    log ${1+"$@"}
+    exit 1
+}
+
+# Get file's inode
+get_inode()
+{
+    local file="$1"
+    if [ "${file}" = "-" ]; then
+        echo 0
+        return
+    fi
+    case `uname -s` in
+        *BSD|Darwin)
+            statfmt="-f %i"
+            ;;
+        *)
+            statfmt="--format=%i"
+            ;;
+    esac
+    stat ${statfmt} "${file}"
+}
+
+# Create a state file 
+create_state_file()
+{
+    local statefile="$1"
+    local logfile="$2"
+    local linenum="$3"
+    local position="$4"
+    local matching="$5"
+    if [ "${linenum}" = "" ]; then
+        linenum="1"
+    fi
+    if [ "${position}" = "" ]; then
+        position="0"
+    fi
+    if [ "${matching}" = "" ]; then
+        matching="false"
+    fi
+    local inode=`get_inode "${logfile}"`
+    printf 'INODENUM="%d"\nLINENUM="%d"\nPOSITION="%d"\nMATCHING="%s"\n' \
+      "${inode}" "${linenum}" "${position}" "${matching}" > "${statefile}"
+}
+
+# Create a state file for an empty file
+reset_state_file()
+{
+    create_state_file "$1" "$2"
+}
+
+# Verify a string
+verify_value()
+{
+    local name="$1"
+    local expected="$2"
+    local actual="$3"
+    [ "${actual}" = "${expected}" ] || errout "ERROR: mismatch for ${name}: expected '${expected}' but got '${actual}'"
+}
+
+# Verify a state file
+verify_state_file()
+{
+    local statefile="$1"
+    local logfile="$2"
+    local linenum="$3"
+    local position="$4"
+    local matching="$5"
+    local inode=`get_inode "${logfile}"`
+    . "${statefile}"
+    verify_value INODENUM "${inode}" "${INODENUM}"
+    verify_value LINENUM "${linenum}" "${LINENUM}"
+    verify_value POSITION "${position}" "${POSITION}"
+    verify_value MATCHING "${matching}" "${MATCHING}"
+}
+
+# Verify expected output
+verify_output()
+{
+    local expected="$1"
+    shift
+    TEMPFILE=`mktemp -q /tmp/logwarntest.XXXXXX`
+    [ $? -eq 0 ] || errout "can't create temporary file"
+    trap "rm -f ${TEMPFILE}" 0 2 3 5 10 13 15
+    "${LOGWARN}" ${1+"$@"} > "${TEMPFILE}"
+    diff -u "${expected}" "${TEMPFILE}" || errout "ERROR: incorrect output from test"
+    trap - 0 2 3 5 10 13 15
+    rm -f "${TEMPFILE}"
+}
+
